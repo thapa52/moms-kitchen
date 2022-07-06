270fc467c854de0cd568e5efe1ab5816178d93a9
thapa52 <thapa.pradeepp@gmail.com>
2022-07-07 03:58:09 +0545

    banner added in app and route added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 import Banner from './Banner'
 import Nav from './Nav'
+import Home from './Home'
 
 const App = () => {
   return (
     <>
       <Nav />
       <Banner />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
